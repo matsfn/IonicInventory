997ca9fa028f12dddb8b6e608c7210ccb4fc0cc9
klitusas <s236649@stud.hioa.no>
2017-02-19 16:37:26 +0100

    Added info field v.1.1.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,8 +21,9 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Page One', component: Page1 },
-      { title: 'Page Two', component: Page2 }
+      { title: 'Inventory', component: Page1 },
+      { title: 'Charts', component: Page2 },
+      { title: 'Control', component: Page2 }
     ];
 
   }
